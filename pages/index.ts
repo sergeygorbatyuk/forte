b72940265a5451308669f632895881291a7309be
Sergii Gorbatiuk <Sergii.Gorbatiuk@tui.co.uk>
2025-06-13 16:27:58 +0300

    add singleton

--- pages/index.ts
+++ pages/index.ts
@@ -2,13 +2,14 @@ import { MainMenuPage } from './main.menu.page';
 import { MovieDetailsPage } from './movie.details.page';
 import { Top250MoviesPage } from './top250.movies.page';
 import { MainPage } from './main.page';
+import { initSingleton } from './init.singleton';
 
 function pageProvider(page) {
   return {
-      mainMenuPage: new MainMenuPage(page),
-      movieDetailsPage: new MovieDetailsPage(page),
-      top250MoviesPage: new Top250MoviesPage(page),
-      mainPage:new MainPage(page)
+      mainMenuPage: initSingleton(MainMenuPage, page),
+      movieDetailsPage: initSingleton(MovieDetailsPage, page),
+      top250MoviesPage: initSingleton(Top250MoviesPage, page),
+      mainPage: initSingleton(MainPage, page)
   }
 }
 
